d4d190ef2e0cc499082fb971e1fdbc2a006d4b76
Elsa-2002 <elsaeldhose2002@gmail.com>
2021-09-17 02:04:40 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "Flutter",
+      home: Scaffold(
+        body: Container(
+          child: Center(
+            child: Text("Welcome"),
+          ),
+        ),
+      ),
+    ),
+  );
+}
